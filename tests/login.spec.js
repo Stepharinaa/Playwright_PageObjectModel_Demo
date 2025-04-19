54cd0502bb7ba27ab996a83c4da1e976d82cf689
Stepharinaa <stephanielplee@gmail.com>
2025-04-19 09:20:59 +0100

    adjust test titles for clarity

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,26 +3,22 @@ import { LoginPage } from "../Pages/login";
 
 // @ts-check
 
-test("Ensure login passes with correct username and password", async ({
-  page,
-}) => {
-  const Login = new LoginPage(page);
+test("should log in successfully with valid credentials", async ({ page }) => {
+  const login = new LoginPage(page);
 
-  await Login.goToLoginPage();
-  await Login.login("tomsmith", "SuperSecretPassword!");
+  await login.goToLoginPage();
+  await login.login("tomsmith", "SuperSecretPassword!");
 
-  await Login.securePage();
+  await login.securePage();
 });
 
-test("Ensure user is met with error message when username and/or password is incorrect", async ({
-  page,
-}) => {
-  const Login = new LoginPage(page);
+test("should show an error with invalid credentials", async ({ page }) => {
+  const login = new LoginPage(page);
 
-  await Login.goToLoginPage();
-  await Login.login("wrongUsername", "SuperSecretPassword!");
+  await login.goToLoginPage();
+  await login.login("wrongUsername", "SuperSecretPassword!");
 
-  await expect(Login.invalid_alert).toBeVisible();
+  await expect(login.invalid_alert).toBeVisible();
 
   await page.close();
 });
